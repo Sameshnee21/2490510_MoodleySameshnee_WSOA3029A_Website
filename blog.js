6293494f6e005afcca9509d762a95fcccfa2f6ca
Sameshnee21 <2490510@students.wits.ac.za>
2023-08-14 12:18:05 +0200

    home page addition
    -text addition
    - 2nd card for blog
    - image alterations

--- blog.js
+++ blog.js
@@ -3,3 +3,9 @@ const card=document.querySelector('.card__inner');
 card.addEventListener('click', function (){
     card.classList.toggle('is-flipped');
 });
+
+const card2=document.querySelector('.card__inner2');
+
+card2.addEventListener('click', function (){
+    card2.classList.toggle('is-flipped');
+});
