94b83271c9aa899cc33833d0946a941f6134669f
Sameshnee21 <2490510@students.wits.ac.za>
2023-09-09 23:06:57 +0200

    Data Viz 1
    -APOD addition
    -quote used as footer

---
+++ dataviz.js
@@ -0,0 +1,71 @@
+const apiKey = 'ahQFiYRloOL65cCcG5Oq19Q1evryc7D4vK0pewih';
+const url = 'https://api.nasa.gov/planetary/apod?';
+
+let container = document.querySelector('.container');
+
+
+
+let button = document.getElementById('fetch-image');
+
+button.addEventListener('click', ()=>{
+    let imageContainer = document.querySelector('.image-container');
+    imageContainer.remove();
+
+let newImageContainer = document.createElement('div');
+newImageContainer.classList.add('image-container');
+
+container.append(newImageContainer);
+
+    let dateInput = document.querySelector('.details-input input');
+    let date = dateInput.value;
+
+    let request = new XMLHttpRequest();
+    request.open('GET', url + "date=" + date  + "&api_key=" + apiKey, true);
+    request.send();
+    request.onload = function(){
+       if(request.status===200){
+       // console.log("Working");
+       let data = JSON.parse(request.responseText);
+       //console.log(data);
+       let imageUrl = data.url;
+       let image = document.createElement('img');
+       image.src = imageUrl;
+       newImageContainer.append(image);
+       }
+       else{
+        window.alert('Please enter the correct format.');
+       }
+    }
+});
+
+let hdButton = document.getElementById('fetch-hd');
+hdButton.addEventListener('click', () => {
+    let imageContainer = document.querySelector('.image-container');
+    imageContainer.remove();
+
+let newImageContainer = document.createElement('section');
+newImageContainer.classList.add('image-container');
+
+container.append(newImageContainer);
+
+    let dateInput = document.querySelector('.details-input input');
+    let date = dateInput.value;
+
+    let request = new XMLHttpRequest();
+    request.open('GET', url + "date=" + date  + "&api_key=" + apiKey, true);
+    request.send();
+    request.onload = function(){
+       if(request.status===200){
+       // console.log("Working");
+       let data = JSON.parse(request.responseText);
+       //console.log(data);
+       let imageUrl = data.hdurl;
+       let image = document.createElement('img');
+       image.src = imageUrl;
+       newImageContainer.append(image);
+       }
+       else{
+        window.alert('Please enter the correct format.');
+       }
+    }
+});
