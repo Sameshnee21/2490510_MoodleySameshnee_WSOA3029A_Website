a28e5910ee60805c045e1197dd29bb802afbc1b6
Sameshnee21 <2490510@students.wits.ac.za>
2023-09-11 15:39:47 +0200

    gallery

--- designsection.js
+++ designsection.js
@@ -1,32 +1,32 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+//const carouselSlide = document.querySelector('.carousel-slide');
+//const carouselImages = document.querySelectorAll('.carousel-slide img');
 
 //buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+//const prevBtn = document.querySelector('#prevBtn');
+//const nextBtn = document.querySelector('#nextBtn');
 
 //counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+//let counter = 1;
+//const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+//carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //button listeners
-nextBtn.addEventListener('click', ()=>{
+//nextBtn.addEventListener('click', ()=>{
  if(counter >= carouselImages.length -1) return;
 carouselSlide.style.transition = "transform 0.4s ease-in-out";
 counter++;
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
+//});
 
-prevBtn.addEventListener('click', ()=>{
+//prevBtn.addEventListener('click', ()=>{
     if(counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    });
+ //   });
 
-    carouselSlide.addEventListener('transitionend', ()=>{
+//    carouselSlide.addEventListener('transitionend', ()=>{
 if(carouselImages[counter].id ==='lastClone'){
     carouselSlide.style.transform = "none";
     counter = carouselImages.length - 2;
@@ -39,6 +39,54 @@ if(carouselImages[counter].id ==='firstClone'){
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 }
 
-    });
+ //   });
 
-
+//selecting all required elements
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".gallery .image");
+window.onload = ()=>{ //after window loaded
+  filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
+    if(selectedItem.target.classList.contains("item")){ //if user selected item has .item class
+      filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+      selectedItem.target.classList.add("active"); //add that active class on user selected item
+      let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); //getting image data-name value
+        //if user selected item data-name value is equal to images data-name value
+        //or user selected item data-name value is equal to "all"
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); //first remove the hide class from the image
+          image.classList.add("show"); //add show class in image
+        }else{
+          image.classList.add("hide"); //add hide class in image
+          image.classList.remove("show"); //remove show class from the image
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+  }
+}
+//fullscreen image preview function
+//selecting all required elements
+const previewBox = document.querySelector(".preview-box"),
+categoryName = previewBox.querySelector(".title p"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+function preview(element){
+  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  }
+}
